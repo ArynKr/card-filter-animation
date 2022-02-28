09d477fd5e5790c0074c0a95e168a87a336972a8
Aryan Kumar <aryankumar1504@gmail.com>
2022-02-28 17:54:14 +0530

    Feat WebApp:Display popular movies names

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [popular, setPopular] = useState([]);
+
+  // Run fetchPopular method for the very first render
+  useEffect(() => {
+    fetchPopular();
+  }, [])
+
+  // Fetch Popular Movies
+  const fetchPopular = async () => {
+    const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB}&language=en-US&page=1`);
+    const data = await res.json();
+    setPopular(data.results);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 onClick={fetchPopular}>Hello</h1>
+      {popular.map((item) => (<p>{item.original_title}</p>))}
     </div>
   );
 }
